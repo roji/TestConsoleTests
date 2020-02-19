9d041fcc7dc30f8ffa3e517f27010d86247a1483
Shay Rojansky <roji@roji.org>
2020-02-19 12:30:59 +0100

    Initial commit

---
+++ NUnitTests/NUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+namespace NUnitTests
+{
+    public class NUnitTests
+    {
+        [Test]
+        public void ConsoleTest()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
